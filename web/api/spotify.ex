52331753d33a01a8400e0f8447f7dc7c9acf72e7
Raymond Zhu <junhao.raymond.zhu@gmail.com>
2016-07-08 16:31:04 -0400

    Processed data from Spotify API.

--- web/api/spotify.ex
+++ web/api/spotify.ex
@@ -1,7 +1,18 @@
 defmodule Karaoke.Spotify do
+  require Logger
+
   defp handle_response({:ok, %{status_code: 200, body: body}}), do: body |> Poison.decode! |> Map.fetch!("tracks")
   defp handle_response({:error, %{reason: reason}}), do: {:error, %{reason: reason}}
   defp handle_response({:ok, %{status_code: code, body: body}}), do: {:error, %{status_code: code, body: body}}
+
+  defp process_response({:error, info}), do: Logger.error "Could not get album art.\n #{inspect info}"
+  defp process_response(spotify_tracks) do
+    map_of_art = %{}
+    for %{"id" => id, "album" => %{"images" => [%{"url" => image_url} | _rest]}} <- spotify_tracks do
+       map_of_art
+       |> Map.put(id, image_url)
+    end
+  end
 
   defp concat_ids(list_of_tracks) do
     list_of_tracks
@@ -30,6 +41,7 @@ defmodule Karaoke.Spotify do
         url
         |> HTTPoison.get
         |> handle_response
+        |> process_response
     end
 
   end
